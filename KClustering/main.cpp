9498207c92d69e636988168bb1d71d9e2d52571c
孙榕鞠 <sunyoumari@MacBook-Pro-2.local>
2014-07-16 22:00:43 +0800

    Initial Commit

---
+++ KClustering/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  KClustering
+//
+//  Created by 孙榕鞠 on 14-7-16.
+//  Copyright (c) 2014年 孙 榕鞠. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
